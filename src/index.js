5309e5eb0d9087bcea4e0d46ec7c277f351e4044
guardianwang <962247283@qq.com>
2022-12-20 16:27:17 -0500

    lift state to Game. Lifting means to change children's state to children's props

--- src/index.js
+++ src/index.js
@@ -29,64 +29,33 @@ function row(matrix, index) {
     return math.flatten(math.subset(matrix, math.index(index, math.range(0, rows)))).toArray();
 }
 
-class Board extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            symbols: ['X', 'O'],
-            playerID: 0,
-            squares: Array(9).fill(null),
-            winner: null,
-        };
-    }
-
-    checkWinner(squares) {
-        const indices = math.range(0, 9);
-        const matrixBoard = math.matrix(math.reshape(indices, [3, 3]));
-        const transposed = math.transpose(matrixBoard);
-        const diagonal = math.matrix([[0, 4, 8], [2, 4, 6]]);
-        const checkIdx = math.concat(matrixBoard, transposed, diagonal, 0);
-        for (let i = 0; i < checkIdx.size()[0]; i++) {
-            let line = row(checkIdx, i);
-            let [sa, sb, sc] = math.subset(squares, math.index(line));
-            if (sa !== null && sa === sb && sb === sc) {
-                return sa;
-            }
+function checkWinner(squares) {
+    const indices = math.range(0, 9);
+    const matrixBoard = math.matrix(math.reshape(indices, [3, 3]));
+    const transposed = math.transpose(matrixBoard);
+    const diagonal = math.matrix([[0, 4, 8], [2, 4, 6]]);
+    const checkIdx = math.concat(matrixBoard, transposed, diagonal, 0);
+    for (let i = 0; i < checkIdx.size()[0]; i++) {
+        let line = row(checkIdx, i);
+        let [sa, sb, sc] = math.subset(squares, math.index(line));
+        if (sa !== null && sa === sb && sb === sc) {
+            return sa;
         }
-        return null;
     }
+    return null;
+}
 
+class Board extends Component {
     renderSquare(i) {
         return <Square 
-                    value={this.state.squares[i]} 
-                    onClick={() => this.handleClick(i)} 
+                    value={this.props.squares[i]} 
+                    onClick={() => this.props.onClick(i)} 
                 />;
     }
 
-    handleClick(i) {
-        if (this.state.winner !== null) {
-            return;
-        }
-        const squares = this.state.squares.slice();
-        squares[i] = this.state.symbols[this.state.playerID];
-        this.setState({
-            squares: squares,
-            playerID: 1 - this.state.playerID,
-            winner: this.checkWinner(squares),
-        });
-    }
-
     render() {
-        let status = '';
-        if (this.state.winner !== null) {
-            status = `Winner: ${this.state.winner}`;
-        } else {
-            status = `Next player: ${this.state.symbols[this.state.playerID]}`;
-        }
-
         return (
             <div>
-                <div className='status'>{status}</div>
                 <div className='board-row'>
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -108,14 +77,55 @@ class Board extends Component {
 }
 
 class Game extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            symbols: ['X', 'O'],
+            playerID: 0,
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            winner: null,
+        };
+    }
+    
+    handleClick(i) {
+        if (this.state.winner !== null) {
+            return;
+        }
+        let history = this.state.history;
+        const squares = history[history.length - 1].squares.slice();
+        squares[i] = this.state.symbols[this.state.playerID];
+        this.setState({
+            history: history.concat({
+                squares: squares,
+            }),
+            playerID: 1 - this.state.playerID,
+            winner: checkWinner(squares),
+        });
+    }
+
     render() {
+        let status = '';
+        if (this.state.winner !== null) {
+            status = `Winner: ${this.state.winner}`;
+        } else {
+            status = `Next player: ${this.state.symbols[this.state.playerID]}`;
+        }
+        const history = this.state.history;
+        const current = history[history.length - 1];
+
         return (
             <div className='game'>
                 <div className='game-board'>
-                    <Board />
+                    <Board 
+                        squares={current.squares}
+                        // Use arrow function instead of simply this.handleClick
+                        onClick={i => this.handleClick(i)}
+                    />
                 </div>
                 <div className='game-info'>
-                    <div></div>
+                    <div className='status'>{status}</div>
                     <ol></ol>
                 </div>
             </div>
